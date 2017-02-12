935a290a1deabf390a544d3602f5316cbf2e8bd7
kanatamon <ponde.moonlord@gmail.com>
2017-02-12 13:44:48 +0700

    Add echo bot, reply message that user send

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 'use strict'
 
 const express = require('express')
+const line = require('node-line-bot-api')
+const bodyParser = require('body-parser')
+
 const app = express()
 
-app.get('/', (request, response) => {
-	response.send(`Hello, I'am bot`)
+const chanelAccessToken = '30oK3U/OYhAREtjGPWjZlQ5ecHRO+w5NmJBBrAT7fysZzfWeeHSRVrcBw5Nx/whRPN13sBSzINT78OyWuL5oRqUNet3qeqbsqEpzBspTmXz/YE5leFQqaCAk/PgyjsRz/pBjApw3ggH8iib1TnMfjQdB04t89/1O/w1cDnyilFU='
+const channelSecret = 'c4e4bee94a4699fafc214a5a725fb3f9'
+
+app.use(bodyParser.json({
+  verify (request, response, buffer) {
+    request.rawBody = buffer
+  }
+}))
+
+// Initialize authentication
+line.init({
+	accessToke: chanelAccessToken,
+	channelSecret: channelSecret
+})
+
+app.post('/webhook/', line.validator.validateSignature(), (request, response, next) => {
+  // Get content from request body
+  const promises = request.body.events.map(event => {
+    // Reply message
+    return line.client
+      .replyMessage({
+        replyToken: event.replyToken,
+        messages: [
+          {
+            type: 'text',
+            text: event.message.text
+          }
+        ]
+      })
+  })
+  
+  Promise
+    .all(promises)
+    .then(() => response.json({success: true}))
 })
 
 app.listen(process.env.PORT || 3000, () => {
