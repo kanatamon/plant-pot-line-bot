3d6225b294550ced44eb73e3ffaaf1eedc449639
kanatamon <ponde.moonlord@gmail.com>
2017-02-12 14:16:07 +0700

    Add ability to get temperature data then reply

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+require('isomorphic-fetch')
+
 const express = require('express')
 const line = require('node-line-bot-api')
 const bodyParser = require('body-parser')
@@ -8,6 +10,17 @@ const app = express()
 
 const channelSecret = 'c4e4bee94a4699fafc214a5a725fb3f9'
 const chanelAccessToken = '30oK3U/OYhAREtjGPWjZlQ5ecHRO+w5NmJBBrAT7fysZzfWeeHSRVrcBw5Nx/whRPN13sBSzINT78OyWuL5oRqUNet3qeqbsqEpzBspTmXz/YE5leFQqaCAk/PgyjsRz/pBjApw3ggH8iib1TnMfjQdB04t89/1O/w1cDnyilFU='
+
+
+const readTemperature = () => {
+	return fetch('https://api.netpie.io/topic/PlantPot2/sensor/LightLevel?auth=Y35OK6MFGX5TQm1:eIjiYDLmUhznX6xMPVCLsVGRg')
+				  .then((response) => {
+			      if (response.status >= 400) {
+		          throw new Error("Bad response from server");
+			      }
+			      return response.json();
+				  })
+}
 
 app.use(bodyParser.json({
   verify (request, response, buffer) {
@@ -21,22 +34,29 @@ line.init({
 	channelSecret: channelSecret
 })
 
+// Event message handler
 app.post('/webhook/', line.validator.validateSignature(), (request, response, next) => {
   // Get content from request body
   const promises = request.body.events.map(event => {
     // Reply message
-    return line.client
-      .replyMessage({
-        replyToken: event.replyToken,
-        messages: [
-          {
-            type: 'text',
-            text: event.message.text
-          }
-        ]
-      })
+    if (event.message.text === 'temperature') {
+    	return readTemperature( json => {
+    		return line.client
+					      .replyMessage({
+					        replyToken: event.replyToken,
+					        messages: [
+					          {
+					            type: 'text',
+					            text: json[0].payload
+					          }
+					        ]
+					      })	
+    	})
+    	
+    }
+
   })
-  
+
   Promise
     .all(promises)
     .then(() => response.json({success: true}))
