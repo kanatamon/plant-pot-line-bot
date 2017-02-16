3bf267d9e521dc9d0352147b822056f3430c26f5
kanatamon <ponde.moonlord@gmail.com>
2017-02-16 23:36:26 +0700

    Try reply type 'template'

--- index.js
+++ index.js
@@ -36,10 +36,37 @@ app.post('/webhook/', line.validator.validateSignature(), (request, response, ne
 					.replyMessage({
 			      replyToken: event.replyToken,
 			      messages: [
+			        // {
+			        //   type: 'text',
+			        //   text: `ตอนนี้อุณหภูมิอยู่ที่ ${json[0].payload} c`
+			        // }
 			        {
-			          type: 'text',
-			          text: `ตอนนี้อุณหภูมิอยู่ที่ ${json[0].payload} c`
-			        }
+							  "type": "template",
+							  "altText": "this is a buttons template",
+							  "template": {
+							      "type": "buttons",
+							      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+							      "title": "Menu",
+							      "text": "Please select",
+							      "actions": [
+							          {
+							            "type": "postback",
+							            "label": "Buy",
+							            "data": "action=buy&itemid=123"
+							          },
+							          {
+							            "type": "postback",
+							            "label": "Add to cart",
+							            "data": "action=add&itemid=123"
+							          },
+							          {
+							            "type": "uri",
+							            "label": "View detail",
+							            "uri": "http://example.com/page/123"
+							          }
+							      ]
+							  }
+							}
 			      ]
 			    })
 				)
