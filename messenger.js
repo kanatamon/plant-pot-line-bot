ae79f8861ab82ec7ba6c79facb25c80792e5a913
kanatamon <ponde.moonlord@gmail.com>
2017-02-18 17:55:42 +0700

    Fix systax error

--- messenger.js
+++ messenger.js
@@ -2,10 +2,12 @@ const api = require('./api')
 const StatusTemplate = require('./components/StatusTemplate')
 
 const replyStatusTemplate = (event, line) => api.getSensorValues()
-	.then(values => line.client.replyMessage({
-		replyToken: event.replyToken,
-		messages: [StatusTemplate(values)]
-	})
+	.then(values => line.client.replyMessage(
+		{
+			replyToken: event.replyToken,
+			messages: [StatusTemplate(values)]
+		}
+	))
 
 
 
